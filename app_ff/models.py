8e2a13f1b3a28b5e3643f3b48291daa641444cfb
anderson <andersondiasmd25@gmail.com>
2025-04-01 15:57:22 -0300

    criado tabela inicial do banco de dados migração

--- app_ff/models.py
+++ app_ff/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),   # Entrada
+        ('expense', 'Expense'), # Saída
+    ]
+
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)  # Income ou Expense
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Valor com até 10 dígitos e 2 casas decimais
+    description = models.CharField(max_length=50)  # Descrição curta
+    created_at = models.DateTimeField(auto_now_add=True)  # Criado automaticamente
+    updated_at = models.DateTimeField(auto_now=True)  # Atualizado automaticamente
+
+    def __str__(self):
+        return f"{self.type} - ${self.amount:.2f}"
+
